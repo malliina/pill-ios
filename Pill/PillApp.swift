b36bb71cd4e97059f4e86b2608494bfb955a0b52
Michael Skogberg <malliina123@gmail.com>
2021-09-25 16:38:25 +0300

    Initial Commit

---
+++ Pill/PillApp.swift
@@ -0,0 +1,17 @@
+//
+//  PillApp.swift
+//  Pill
+//
+//  Created by Michael Skogberg on 25.9.2021.
+//
+
+import SwiftUI
+
+@main
+struct PillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
